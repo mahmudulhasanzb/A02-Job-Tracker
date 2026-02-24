4aafd367b1b24ddfec2d0704815c9e0103b48af9
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-02-24 14:39:39 +0600

    empty function added. it will work something like- if there is no job available show the emptycar

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
 const allCards = document.getElementById('allCards');
+let jobs = document.getElementById('jobs');
+let jobsLength = allCards.children.length;
 
-// total count
+
+// count variables
 let total = document.getElementById('totalCount');
 let interviewCount = document.getElementById('interviewCount');
 let rejectedCount = document.getElementById('rejectedCount');
+
+// count objects
+let interviews = [];
+let rejected = [];
+
+// count calculation
+jobs.innerText = jobsLength + ' jobs';
+interviewCount.innerText = interviews.length;
+rejectedCount.innerText = rejected.length;
 
 // total count function
 function totalCount() {
@@ -12,10 +24,19 @@ function totalCount() {
 
 totalCount();
 
+// show empty card function when nothing available
+function showEmptyCard() {
+  const noJobSection = document.getElementById('noJobSection');
+  noJobSection.classList.remove('hidden');
+}
+
+
 // toggle filter button
 const allFilterBtn = document.getElementById('allBtn');
 const interviewFilterBtn = document.getElementById('interviewBtn');
 const rejectedFilterBtn = document.getElementById('rejectedBtn');
+
+
 
 // toggle filter button function
 function toggleStyle(id) {
@@ -34,5 +55,38 @@ function toggleStyle(id) {
   // selected.classList.remove('bg-base-100')
   //add style to the specific button
   selected.classList.add('btn-primary')
+
+
+
+  if (id === 'interviewBtn') {
+    allCards.classList.add('hidden');
+    // const filteredSection = document.getElementById('filtered-section');
+    // filteredSection.classList.remove('hidden');
+    jobs.innerText = interviews.length + ' of ' + jobsLength + ' jobs';
+    
+    // show the empty card when nothing available
+    if (interviews.length == 0) {
+      showEmptyCard();
+      return;
+    }
+  }
+
+  else if (id === 'rejectedBtn') {
+    allCards.classList.add('hidden');
+    // const filteredSection = document.getElementById('filtered-section');
+    // filteredSection.classList.remove('hidden');
+    jobs.innerText = rejected.length + ' of ' + jobsLength + ' jobs';
+
+    // show the empty card when nothing available
+    if (rejected.length === 0) {
+      showEmptyCard();
+      return;
+    }    
+  }
+
+  else {
+    allCards.classList.remove('hidden');
+    noJobSection.classList.add('hidden');
+  }
 
 }

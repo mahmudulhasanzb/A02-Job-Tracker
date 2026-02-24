d4f53af7aa259d11c2e172300e14a4f77a08d160
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-02-24 12:34:47 +0600

    toggle btn function added

--- script/script.js
+++ script/script.js
@@ -1,11 +1,25 @@
-const allCards = document.getElementById('allCards')
-const totalCount = document.getElementById('total')
 
-// total job count
-const updatedTotalCount = totalCount.innerText = allCards.children.length;
+// toggle filter button
+const allFilterBtn = document.getElementById('allBtn');
+const interviewFilterBtn = document.getElementById('interviewBtn');
+const rejectedFilterBtn = document.getElementById('rejectedBtn');
 
-// if there is no job then show the empty page
-if (updatedTotalCount === 0) {
-  const noJob = document.getElementById('noJob');
-  noJob.classList.remove('hidden')
+// toggle filter button function
+function toggleStyle(id) {
+  // remove styles when click any button
+  allFilterBtn.classList.remove('btn-primary')
+  interviewFilterBtn.classList.remove('btn-primary')
+  rejectedFilterBtn.classList.remove('btn-primary')
+
+  // add style when click any button
+  // allFilterBtn.classList.add('bg-base-100')
+  // interviewFilterBtn.classList.add('bg-base-100')
+  // rejectedFilterBtn.classList.add('bg-base-100')
+
+  // specify the a button
+  const selected = document.getElementById(id);
+  // selected.classList.remove('bg-base-100')
+  //add style to the specific button
+  selected.classList.add('btn-primary')
+
 }
